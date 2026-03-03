9f5439413c3da8628240b5adb3de6aae3bf4f9a3
sonali <sonalik.ug23.cs@nitp.ac.in>
2026-03-04 01:51:21 +0530

    change env for correcting inngest spelling

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -10,8 +10,8 @@ export const ENV = {
     DB_URI: process.env.DB_URI,
     NODE_ENV: process.env.NODE_ENV,
     CLIENT_URL: process.env.CLIENT_URL,
-    INGEST_EVENT_KEY: process.env.INGEST_EVENT_KEY,
-    INGEST_SIGNING_KEY: process.env.INGEST_SIGNING_KEY,
+    INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
     STREAM_API_KEY: process.env.STREAM_API_KEY,
     STREAM_API_SECRET: process.env.STREAM_API_SECRET,
 };
