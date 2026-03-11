a3988f8918ac8d9c33980103a11d0f82fa9b6d5a
sonali <sonalik.ug23.cs@nitp.ac.in>
2026-03-11 22:35:19 +0530

    fixing session

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
-    withCredentials: true, // browser will send cookies to server automatically on every single request
-    
+  baseURL: (import.meta.env.VITE_API_URL || "http://localhost:5000") + "/api",
+  withCredentials: true,
 });
 
 export default axiosInstance;
